32b1eba3afd5cab4a90d16d284a5e89a4de3ee36
masterUNG <phrombutr@gmail.com>
2015-12-04 17:31:04 +0700

    Re Design

--- app/src/main/java/com/example/administrator/khonnkean/im_wat.java
+++ app/src/main/java/com/example/administrator/khonnkean/im_wat.java
@@ -1,18 +1,14 @@
 package com.example.administrator.khonnkean;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class im_wat extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_im_wat);
+        setContentView(R.layout.activity_im_wat_v1);
 
     }
 
